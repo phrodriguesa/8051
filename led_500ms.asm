34408f1c4d023d6ad07082754aa569a69d1cf9d7
phrrodrigues <142232518+phrodriguesa@users.noreply.github.com>
2026-04-08 23:57:52 -0300

    Update led_500ms.asm

--- led_500ms.asm
+++ led_500ms.asm
@@ -1,42 +1,27 @@
-ORG 0000H            
+ORG 0000H
 
 main:
-    CLR P1.0         ; Coloca o pino P1.0 em nível baixo (LED desligado)
+    CPL P1.0         ; alterna o LED
 
-    NOP              ; Atraso de 1 us
-    NOP              ; Atraso de 1 us
+    CALL DELAY       ; espera ~500 ms
 
-    CALL DELAY       ; Chama a rotina de atraso (~500 ms)
-
-    SETB P1.0        ; Coloca P1.0 em nível alto (LED liga) (adicione um breakpoint aqui)
-
-STOP:
-    SJMP STOP        ; Loop infinito para travar o programa (evita reinício)
-
+    SJMP main        ; repete
 
 ; DELAY (~500 ms)
 DELAY:
-    MOV R2,#5        ; Contador externo (repete tudo 5 vezes)
+    MOV R2,#5        ; Contador externo
 
 DL2:
-    MOV R1,#199; Contador intermediário (200 repetições)
+    MOV R1,#201      ; Ajustado (+1 para compensar R3 removido)
 
 DL1:
-    MOV R0,#249      ; Contador interno (249 repetições)
-
-    NOP              ; Ajuste fino de tempo (1 us)
+    MOV R0,#249      ; Contador interno
 
 DL0:
-    DJNZ R0,DL0      ; Loop interno: decrementa R0 até zero (~498 us total)
+    DJNZ R0,DL0      ; Loop interno
 
-    DJNZ R1,DL1      ; Repete o loop interno 200 vezes
+    DJNZ R1,DL1      ; Loop intermediário
 
-    DJNZ R2,DL2      ; Repete todo o conjunto 5 vezes (~499 ms acumulados)
+    DJNZ R2,DL2      ; Loop externo (~500 ms total)
 
-    MOV R3,#250      ; Define contador extra
-    DJNZ R3,$        ; Loop curto (~500 us)
-
-    MOV R4,#248      ; Outro ajuste fino
-    DJNZ R4,$        ; Loop curto (~496 us)
-
-    RET              ; Retorna para o programa principal
+    RET
